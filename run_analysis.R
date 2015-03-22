6985bf99a7489a823f5f4899f79b1103e592ec5a
shank86 <shank86@gmail.com>
2015-03-23 03:32:12 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -11,19 +11,19 @@ library(reshape2)
 root.dir <- "UCI HAR Dataset"
 data.set <- list()
 
-message("loading features.txt")
+#load features
 data.set$features <- read.table(paste(root.dir, "features.txt", sep="/"), col.names=c('id', 'name'), stringsAsFactors=FALSE)
 
-message("loading activity_features.txt")
+#load activity labels
 data.set$activity_labels <- read.table(paste(root.dir, "activity_labels.txt", sep="/"), col.names=c('id', 'Activity'))
 
 
-message("loading test set")
+#load test sets
 data.set$test <- cbind(subject=read.table(paste(root.dir, "test", "subject_test.txt", sep="/"), col.names="Subject"),
                        y=read.table(paste(root.dir, "test", "y_test.txt", sep="/"), col.names="Activity.ID"),
                        x=read.table(paste(root.dir, "test", "x_test.txt", sep="/")))
 
-message("loading train set")
+#load train sets
 data.set$train <- cbind(subject=read.table(paste(root.dir, "train", "subject_train.txt", sep="/"), col.names="Subject"),
                         y=read.table(paste(root.dir, "train", "y_train.txt", sep="/"), col.names="Activity.ID"),
                         x=read.table(paste(root.dir, "train", "X_train.txt", sep="/")))
@@ -44,20 +44,18 @@ rename.features <- function(col) {
   return(col)
 }
 
-## Extracts only the measurements on the mean and standard deviation for each measurement.
-
 tidy <- rbind(data.set$test, data.set$train)[,c(1, 2, grep("mean\\(|std\\(", data.set$features$name) + 2)]
 
-## Uses descriptive activity names to name the activities in the data set
+## name activities in data set
 
 names(tidy) <- c("Subject", "Activity.ID", rename.features(data.set$features$name[grep("mean\\(|std\\(", data.set$features$name)]))
 
-## Appropriately labels the data set with descriptive activity names.
+## label  data set with activity names.
 
 tidy <- merge(tidy, data.set$activity_labels, by.x="Activity.ID", by.y="id")
 tidy <- tidy[,!(names(tidy) %in% c("Activity.ID"))]
 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Create seconddata set with the average of each variable for each activity and each subject.
 
 tidy.mean <- ddply(melt(tidy, id.vars=c("Subject", "Activity")), .(Subject, Activity), summarise, MeanSamples=mean(value))
 
